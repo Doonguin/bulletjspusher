ae3c150da96788f6ccda4687f1a2bf84df6ce11f
Doonguin <penguinicesp@gmail.com>
2025-10-22 23:26:32 +0200

    Wow, I wrote some basic code just fill up the file a bit! WOAH!

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-// Initial commit BS or sum
+class Point {
+    readonly x: number;
+    readonly y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    get getCoordinates() {
+        return `X-axis: ${this.x}, Y-axis: ${this.y}`
+    }
+}
+
+const point = new Point(10, 25);
+console.log(point.getCoordinates);
