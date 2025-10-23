af4aa305338e7f4247fff489b579e374aa468de4
Doonguin <penguinicesp@gmail.com>
2025-10-23 18:56:02 +0200

    Trying to figure out a good way to set this shit up

--- src/calls/Push.ts
+++ src/calls/Push.ts
@@ -4,3 +4,13 @@ export interface PushData {
     type: PushType;
     [key: string]: any;
 }
+
+export interface PushOptions {
+    url: string;
+    apiKey: string;
+    data: PushData;
+}
+
+export async function pushGeneric(options: PushOptions) {
+    const { apiKey, url, data } = options;
+}
